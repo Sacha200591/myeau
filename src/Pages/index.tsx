e10a2abb381f5b0c27d875e96b990cfc2f7dcd70
Sacha29032005 <sachamarechal@yahoo.com>
2025-02-13 15:47:49 +0100

    Initial commit

---
+++ src/Pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Myeau" description="Description..."/>
+      <h1>Hello world</h1>
+    </>
+  );
+}
